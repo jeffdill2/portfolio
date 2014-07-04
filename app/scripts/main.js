291a9936d3fabd1c768a34fba756eac34766d250
Jeffrey Dill <jeffdill2@gmail.com>
2014-07-04 19:29:34 -0400

    Project popups working -- added dynamic nav bar -- various formatting changes

---
+++ app/scripts/main.js
@@ -0,0 +1,33 @@
+$('.project-preview').click(function() {
+	$('.project-detail-backdrop').show();
+	$('.project-detail').prepend('<div class="close-button"></div>')
+	$('.project-detail').show();
+});
+
+$(document).on('click', '.close-button', function() {
+	$('.project-detail').html('');
+	$('.project-detail').hide();
+	$('.project-detail-backdrop').hide();
+});
+
+var aryNavLinks = ['#about-link', '#portfolio-link', '#contact-link'];
+
+aryNavLinks.forEach(function(link) {
+	$(link).click(function() {
+		$('html, body').animate({scrollTop: $(link + '-to').offset().top - 20}, 'slow');
+	});
+});
+
+$(window).scroll(function() {
+	$('.dynamic-header').hide();
+
+    clearTimeout($.data(this, "scrollCheck"));
+
+    $.data(this, "scrollCheck",
+    	setTimeout(function() {
+    		if ($(window).scrollTop() > 710) {
+    			$('.dynamic-header').fadeIn();
+    		}
+    	}, 250)
+    );
+});
