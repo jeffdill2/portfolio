e8e040a5ca230aac39d5919cbd90c911af1952d9
Jeffrey Dill <jeffdill2@gmail.com>
2014-07-05 16:10:54 -0400

    Added templating for project details view -- refactored JS to take advantage of templates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,86 @@
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////// TEMPLATES
+////////////////////////////////////////////////////////////
+var projectDetailTemplate = _.template($('#project-detail-template').text());
+
+////////////////////////////////////////////////////////////
+//////////////////////////////////////////// PROJECT DETAILS
+////////////////////////////////////////////////////////////
+var objProsAndConsDetails = {
+	desktopPhoto: '../images/pros-and-cons-desktop.png',
+	mobilePhoto: '../images/pros-and-cons-mobile.png',
+	description: 'Pros and Cons is badass!',
+	techs: [
+		'test1',
+		'test2',
+		'test3'
+	]
+};
+
+var objChatAtchaDetails = {
+	desktopPhoto: 'test',
+	mobilePhoto: 'test',
+	description: 'test',
+	techs: [
+		'test1',
+		'test2',
+		'test3'
+	]
+};
+
+var objImageGalleryDetails = {
+	desktopPhoto: 'test',
+	mobilePhoto: 'test',
+	description: 'test',
+	techs: [
+		'test1',
+		'test2',
+		'test3'
+	]
+};
+
+var objCSSAnimationsDetails = {
+	desktopPhoto: 'test',
+	mobilePhoto: 'test',
+	description: 'test',
+	techs: [
+		'test1',
+		'test2',
+		'test3'
+	]
+};
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////// PROJECT FUNCTIONS
+////////////////////////////////////////////////////////////
 $('.project-preview').click(function() {
-	$('.project-detail-backdrop').show();
-	$('.project-detail').prepend('<div class="close-button"></div>')
-	$('.project-detail').show();
+	var renderedTemplate;
+
+	switch (this.id) {
+		case 'project-pros-and-cons':
+			renderedTemplate = projectDetailTemplate(objProsAndConsDetails);
+			break;
+
+		case 'project-chat-atcha':
+			renderedTemplate = projectDetailTemplate(objChatAtchaDetails);
+			break;
+
+		case 'project-image-gallery':
+			renderedTemplate = projectDetailTemplate(objImageGalleryDetails);
+			break;
+
+		case 'project-css-animations':
+			renderedTemplate = projectDetailTemplate(objCSSAnimationsDetails);
+			break;
+
+		default:
+			break;
+	}
+
+	$('.project-detail').append('<div class="close-button"></div>')
+	$('.project-detail').append(renderedTemplate);
+	$('.project-detail-backdrop').fadeIn();
+	$('.project-detail').fadeIn();
 });
 
 $(document).on('click', '.close-button', function() {
@@ -10,6 +89,9 @@ $(document).on('click', '.close-button', function() {
 	$('.project-detail-backdrop').hide();
 });
 
+////////////////////////////////////////////////////////////
+//////////////////////////////////////////// NAVIGATION BARS
+////////////////////////////////////////////////////////////
 var aryNavLinks = ['home-link', 'about-link', 'portfolio-link', 'contact-link', 'skills-link'];
 
 aryNavLinks.forEach(function(link) {
